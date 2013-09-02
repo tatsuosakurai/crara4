302b376a63f2b3da948be563398ea28199033a57
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2013-09-02 22:39:47 +0900

    Add home#index

--- chapter1/pdf_renderer/test/dummy/config/routes.rb
+++ chapter1/pdf_renderer/test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Dummy::Application.routes.draw do
+  get "/home", to: "home#index", as: :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

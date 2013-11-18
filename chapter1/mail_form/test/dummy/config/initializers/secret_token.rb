8322cea4f2e50a98308c2d8c9f93e8789518c168
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2013-11-18 22:17:03 +0900

    rails plugin new mail_form

---
+++ chapter1/mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '752ebcd718b36b6b61dd12336cdf2fb7f2781c8d1c6c9f6f315215fe3bc5ad3c72293b6695d8d4fe3912447707b7367d32a3f54ab63c9f07ef5ddafd44f817e9'

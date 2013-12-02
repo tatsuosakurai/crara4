6c8a9876aa480c3d14bf20e8306854d7b8ca69ff
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2013-12-02 22:18:24 +0900

    Add attributes?

--- chapter2/mail_form/lib/mail_form/base.rb
+++ chapter2/mail_form/lib/mail_form/base.rb
@@ -3,6 +3,8 @@ module MailForm
     include ActiveModel::AttributeMethods
     attribute_method_prefix 'clear_'
 
+    attribute_method_suffix '?'
+
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
@@ -11,6 +13,10 @@ module MailForm
     protected
     def clear_attribute(attribute)
       send("#{attribute}=", nil)
+    end
+
+    def attribute?(attribute)
+      send(attribute).present?
     end
   end
 end

b0d9eca449b0120eb473c2e7b520eeff5964bd6f
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2014-01-06 23:19:17 +0900

    Fix test

--- chapter2/mail_form/lib/mail_form/base.rb
+++ chapter2/mail_form/lib/mail_form/base.rb
@@ -2,6 +2,10 @@ module MailForm
   class Base
     include ActiveModel::AttributeMethods
     include ActiveModel::Conversion
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+    include ActiveModel::Validations
+
     attribute_method_prefix 'clear_'
 
     attribute_method_suffix '?'
@@ -9,6 +13,10 @@ module MailForm
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
+    end
+
+    def persisted?
+      false
     end
 
     protected

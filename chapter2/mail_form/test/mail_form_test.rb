b78db358302808ec05756bd9a170157a56dca2f8
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2014-02-03 23:59:52 +0900

    temp

--- chapter2/mail_form/test/mail_form_test.rb
+++ chapter2/mail_form/test/mail_form_test.rb
@@ -32,4 +32,21 @@ class MailFormTest < ActiveSupport::TestCase
     sample.email = ""
     assert !sample.email?
   end
+
+  setup do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  test "delivers an email with attributes" do
+    sample = SampleMail.new
+    # Simulate data from the form
+    sample.email = "user@example.com"
+    sample.deliver
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    mail = ActionMailer::Base.deliveries.last
+
+    assert_equal ["user@example.com"], mail.from
+    assert_match "Email: user@example.com", mail.body.encoded
+  end
 end
